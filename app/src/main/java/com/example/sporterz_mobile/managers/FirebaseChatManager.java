efc88077d2453bbb73b763ffbf09e513e363f38b
ilyasabdellaoui <ilyas.abdellaoui21@gmail.com>
2024-05-20 14:06:16 +0100

    [FIX] Chats: fix chat name

--- app/src/main/java/com/example/sporterz_mobile/managers/FirebaseChatManager.java
+++ app/src/main/java/com/example/sporterz_mobile/managers/FirebaseChatManager.java
@@ -74,8 +74,14 @@ public class FirebaseChatManager {
         });
     }
 
-    // Get current user fullname
-    public String getCurrentUserFullName() {
+    // Load chats where the current user is a participant
+    public void loadChats(final ChatLoadListener listener) {
+        if (mCurrentUser == null) {
+            Log.e(TAG, "loadChats: Current user is null");
+            return;
+        }
+
+        // Get current user fullname
         DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
         userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -92,15 +98,6 @@ public class FirebaseChatManager {
                 Log.e("Chat List Adapter", "onCancelled: " + error.getMessage());
             }
         });
-        return cFullName;
-    }
-
-    // Load chats where the current user is a participant
-    public void loadChats(final ChatLoadListener listener) {
-        if (mCurrentUser == null) {
-            Log.e(TAG, "loadChats: Current user is null");
-            return;
-        }
 
         Query chatQuery = mDatabase.child("chats").orderByChild("participants/" + mCurrentUser.getUid()).equalTo(true);
         chatQuery.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -116,16 +113,15 @@ public class FirebaseChatManager {
                     // get the last message in the chat
                     assert chatId != null;
 
-                    String fullName = getCurrentUserFullName();
                     // chat name is composed of : current user fullname # other user fullname
                     // I need to get the current user fullname from string Fullname and change chat name to just other user fullname
                     String[] parts = chat.getName().split("#");
                     String part1 = parts[0];
                     String part2 = parts[1];
-                    if (part1.equals(fullName)) {
-                        chat.setName(part2);
-                    } else {
+                    if (part2.equals(cFullName)) {
                         chat.setName(part1);
+                    } else {
+                        chat.setName(part2);
                     }
 
                     DataSnapshot message = chatSnapshot.child("messages");
